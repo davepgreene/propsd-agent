7816b8df2159b3f77819a5c6f25ae02595f2266e
Dave Greene <davepgreene@gmail.com>
2017-10-31 07:58:23 -0400

    Added http router.

---
+++ http/handler.go
@@ -0,0 +1,81 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/davepgreene/propsd-agent/utils"
+	"github.com/gorilla/mux"
+	"github.com/meatballhat/negroni-logrus"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/thoas/stats"
+	"github.com/urfave/negroni"
+	"github.com/davepgreene/propsd-agent/sources"
+	"github.com/justinas/alice"
+)
+
+// Handler returns an http.Handler for the API.
+func Handler(metadata *sources.Metadata) {
+	r := mux.NewRouter()
+	statsMiddleware := stats.New()
+	r.HandleFunc("/stats", newAdminHandler(statsMiddleware).ServeHTTP)
+
+	v1 := r.PathPrefix("/v1").Subrouter()
+	v1.HandleFunc("/metadata", newMetadataHandler(metadata).ServeHTTP)
+
+	// Core handlers
+	//v1.HandleFunc("/health", newHealthHandler(storage).ServeHTTP)
+	//v1.HandleFunc("/status", newStatusHandler(storage))
+
+	chain := alice.New(metadataMiddleware(metadata))
+
+	// Conqueso handler
+	conquesoProxy := proxy(viper.GetString("propsd.conqueso"))
+	v1.Handle("/conqueso", chain.Append(conquesoProxy).ThenFunc(newConquesoHandler().ServeHTTP))
+
+	// Properties handlers
+	propsProxy := proxy(viper.GetString("propsd.properties"))
+	v1.Handle("/properties", chain.Append(propsProxy).ThenFunc(newPropertiesHandler().ServeHTTP))
+
+	// Define our 404 handler
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// Add middleware handlers
+	n := negroni.New()
+	n.Use(negroni.NewRecovery())
+
+	if viper.GetBool("log.requests") {
+		n.Use(negronilogrus.NewCustomMiddleware(utils.GetLogLevel(), utils.GetLogFormatter(), "requests"))
+	}
+
+	n.Use(statsMiddleware)
+	n.UseHandler(r)
+
+	// Set up connection
+	conn := fmt.Sprintf("%s:%d", viper.GetString("service.host"), viper.GetInt("service.port"))
+	log.Info(fmt.Sprintf("Listening on %s", conn))
+
+	// Bombs away!
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		Addr:              conn,
+		Handler:           n,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	shutdown(server)
+}
+
+// notFoundHandler provides a standard response for unhandled paths
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+
+	w.Write([]byte(""))
+}

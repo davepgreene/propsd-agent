17a309864f2020316c3b26adbd5c87a3c5dc3625
Dave Greene <davepgreene@gmail.com>
2017-10-31 07:58:51 -0400

    Added stats route.

---
+++ http/admin.go
@@ -0,0 +1,29 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/thoas/stats"
+)
+
+type adminHandler struct {
+	Stats *stats.Stats
+}
+
+func newAdminHandler(s *stats.Stats) http.Handler {
+	return handlers.MethodHandler{
+		"GET": &adminHandler{
+			Stats: s,
+		},
+	}
+}
+
+func (h *adminHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	b, _ := json.Marshal(h.Stats.Data())
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}

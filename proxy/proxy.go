1b2cb19f06be92fcee4d3de36e1e4d8a7b7eb530
Dave Greene <davepgreene@gmail.com>
2017-11-12 23:05:16 -0500

    Added some logic around exposing issues with the upstream connection to later middleware functions.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -3,15 +3,18 @@ package proxy
 import (
 	"net/url"
 	"net/http"
-	"github.com/vulcand/oxy/testutils"
 	"time"
 	"io/ioutil"
 	"strings"
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	UpstreamHeader = "X-Upstream-Proxy-Invalid"
+)
+
 type Proxy struct {
-	url *url.URL
+	url string
 	handler http.Handler
 	client http.Client
 	data string
@@ -23,7 +26,7 @@ func New(url string, handler http.Handler) *Proxy {
 	}
 
 	return &Proxy{
-		url: testutils.ParseURI(url),
+		url: url,
 		client: client,
 		handler: handler,
 		data: "",
@@ -33,18 +36,35 @@ func New(url string, handler http.Handler) *Proxy {
 func (p *Proxy) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	req, err := http.NewRequest("GET", p.url.String(), r.Body)
-	if err != nil {
-		// generating the request failed
-	}
+	// We can swallow any errors in request creation because the only ones we could generate
+	// are invalid URLs. If that's the case, we should let error handling for the http client
+	// making the request take care of any issues. That way the client (the one connecting to
+	// this agent) completes the request.
+	req, _ := http.NewRequest("GET", p.url, r.Body)
 
 	var bodyStr string
 
 	resp, err := p.client.Do(req)
+
 	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-		}).Warn("Error connecting to proxied target. Falling back to cached data.")
+		// We need to know what kind of error we're running into. If it's a url.Error,
+		// this isn't an issue upstream, it's a config issue.
+		switch t := err.(type) {
+		case *url.Error:
+			log.WithFields(log.Fields{
+				"error": t,
+			}).Error("Upstream URL cannot be parsed.")
+		default:
+			log.WithFields(log.Fields{
+				"error": t,
+			}).Warn("Error connecting to proxied target. Falling back to cached data.")
+		}
+
+		// Because the stats middleware injects its own ResponseWriter implementation, we
+		// can't just wrap http.ResponseWriter in our own implementation where we track the
+		// state of the upstream server. Instead we have to write a header to the response
+		// as a flag.
+		rw.Header().Add(UpstreamHeader, "true")
 		bodyStr = p.data
 	} else {
 		defer resp.Body.Close()
